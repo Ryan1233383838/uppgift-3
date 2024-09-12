a16acb739f80744b3a3198a585fbeb9bd2326911
ryhu06001 <ryhu06001@E-PW05CGRC>
2024-09-12 13:49:13 +0200

    Add project files.

---
+++ övining 3/Program.cs
@@ -0,0 +1,23 @@
+﻿using System; 
+namespace Uppgift_1._3
+{ 
+    class Program
+    {
+    static void Main(string[] args)
+    {
+            Console.WriteLine("\"Hej\" hörde jag någon på stan.");
+            Console.WriteLine("Efter en stund svarade någon annan också med ett \"Hej!\".");
+    }
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
